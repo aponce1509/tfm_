aed159ca49491b421a487b93c0e00952a425af7d
Alejandro Ponce Miguela <aponce1509@hotmail.com>
2022-05-15 15:23:43 +0200

    minor

--- scripts/ajuste_y/run_0.py
+++ scripts/ajuste_y/run_0.py
@@ -5,7 +5,7 @@ import torchvision.transforms as T
 # setting path
 from exp_p_y.params import params
 from utils_test import *
-from model_run import fix_seed, basic_ipeline, optuna_pipeline
+from model_run import fix_seed, basic_pipeline, optuna_pipeline
 exp_name = "Pruebas tamaño/resolución"
 
 def main(params, torch_rand=True):
@@ -23,7 +23,7 @@ params['n_epochs'] = 10
 params['cube_shape_x'] = 250
 modelo_simple(main, params)
 # modelo_complejo(main, params)
-eff_0(main, params)
+# eff_0(main, params)
 # eff_2(main, params)
 # eff_5(main, params)
 # eff_7(main, params)
