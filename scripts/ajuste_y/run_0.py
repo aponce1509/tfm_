ab68919cd8deaa1809255a26b03ab64af467f1b2
aponce1509 <mialumno@invidere.ugr.es>
2022-05-23 05:42:38 -0400

    modificación completa

--- scripts/ajuste_y/run_0.py
+++ scripts/ajuste_y/run_0.py
@@ -6,7 +6,9 @@ import torchvision.transforms as T
 from exp_p_y.params import params
 from utils_test import *
 from model_run import fix_seed, basic_pipeline, optuna_pipeline
-exp_name = "Pruebas tamaño/resolución"
+# exp_name = "Pruebas modelo complejo"
+# exp_name = "Pruebas tamaño/resolución"
+exp_name = "BORRAR"
 
 def main(params, torch_rand=True):
     fix_seed(params, torch_rand)
@@ -21,35 +23,93 @@ def main_optuna(n_trials):
 # cx = 500
 params['n_epochs'] = 10
 
+params['seeds'] = (3, )
+params['seed_'] = 3
 
 params['cube_shape_x'] = 1500
-# modelo_simple(main, params)
+params['win_shape'] = (62, 62, 128)
+# params['batch_size'] = 
 # modelo_complejo(main, params)
-eff_0(main, params)
-eff_2(main, params)
-eff_5(main, params)
+bs = 64
+step = 3
+gamma = 1.
+lr = 0.0001
+params['batch_size'] = bs
+params['scheduler_gamma'] = gamma
+params['scheduler_step_size'] = step
+
+params['cube_shape_x'] = 1000
+# params['win_shape'] = (62, 62, 128)
+params['batch_size'] = 48
+params['scheduler_gamma'] = 0.7
+params['scheduler_step_size'] = 3
+modelo_simple(main, params)
+
+params['batch_size'] = bs
+params['scheduler_gamma'] = gamma
+params['scheduler_step_size'] = step
+modelo_complejo(main, params)
+
+params['cube_shape_x'] = 1500
+# params['win_shape'] = (62, 62, 128)
+params['batch_size'] = 48
+params['scheduler_gamma'] = 0.7
+params['scheduler_step_size'] = 3
+modelo_simple(main, params)
+params['batch_size'] = bs
+params['scheduler_gamma'] = gamma
+params['scheduler_step_size'] = step
+modelo_complejo(main, params)
+
+
+# params['cube_shape_x'] = 250
+# # params['seeds'] = (19, )
+# # params['seed_'] = 19
+# # params['win_shape'] = (62, 62, 128)
+# # modelo_simple(main, params)
+# modelo_complejo(main, params)
+
+# params['cube_shape_x'] = 500
+# # params['win_shape'] = (62, 62, 128)
+# # modelo_simple(main, params)
+# modelo_complejo(main, params)
+
+
+
+# params['cube_shape_x'] = 2000
+# # params['win_shape'] = (62, 62, 128)
+# # modelo_simple(main, params)
+# modelo_complejo(main, params)
+
+# params['win_shape'] = (224, 62, 224)
+# eff_0(main, params)
+# eff_2(main, params)
+# eff_5(main, params)
 # # eff_7(main, params)
 
 params['cube_shape_x'] = 2000
-modelo_simple(main, params)
-modelo_complejo(main, params)
-eff_0(main, params)
-eff_2(main, params)
-eff_5(main, params)
+# modelo_simple(main, params)
+# modelo_complejo(main, params)
+# params['win_shape'] = (224, 62, 224)
+# eff_0(main, params)
+# eff_2(main, params)
+# eff_5(main, params)
 # # eff_7(main, params)
 
 params['cube_shape_x'] = 250
 # modelo_simple(main, params)
 # modelo_complejo(main, params)
+# params['win_shape'] = (224, 62, 224)
 # eff_0(main, params)
-eff_2(main, params)
+# eff_2(main, params)
 # eff_5(main, params)
 # eff_7(main, params)
 
-params['cube_shape_x'] = 500
+# params['cube_shape_x'] = 500
 # modelo_simple(main, params)
 # modelo_complejo(main, params)
+# params['win_shape'] = (224, 62, 224)
 # eff_0(main, params)
-eff_2(main, params)
+# eff_2(main, params)
 # eff_5(main, params)
 # # eff_7(main, params)
