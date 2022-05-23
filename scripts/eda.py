6c5f8cd10a5973164d7a34bfeafaf97a2844cc9f
Alejandro Ponce Miguela <aponce1509@hotmail.com>
2022-05-23 11:36:17 +0200

    eda

--- scripts/eda.py
+++ scripts/eda.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from graficas_pdf import vistas_detector
 import seaborn as sns
-
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Helvetica"],
+    "font.size": 15
+})
 # %%
 # Leemos los datos
 all_events = pd.concat(
@@ -20,17 +25,26 @@ a = all_events.drop(["Energy", "hitsCharge"], axis=1).\
     groupby(["Event", "PDGcode"])
 energy = all_events_grouped.max().Energy
 ranges = a.max() - a.min()
-ranges.describe()
+#%%
+
+print(ranges.describe().drop('index', axis=1).to_latex())
 # %% Hacemos los histogramas del rango 
 plt.hist(ranges["hitsX"], histtype='step', bins=50, label='x')
 plt.hist(ranges["hitsY"], histtype='step', bins=50, label='y')
 plt.hist(ranges["hitsZ"], histtype='step', bins=50, label='z')
 plt.legend()
+plt.title('Distribución de los eventos en el rango')
+plt.xlabel('Rango (cm)')
+plt.ylabel('Frecuencia')
+plt.savefig('latex/img/hist_rango.pdf')
 # %%
 all_events.describe()
 # %% Histográma de las energías
-plt.hist(energy)
-
+plt.hist(energy, alpha=0.5, bins=20)
+plt.title('Distribución de los eventos en energía')
+plt.xlabel('Energía, E (GeV)')
+plt.ylabel('Frecuencia')
+plt.savefig('latex/img/hist_en.pdf')
 # %%
 vistas_detector(all_events, 1, 11, False)
 # %%
@@ -40,17 +54,17 @@ energy = all_events.groupby(["Event", "PDGcode"]).mean().Energy
 counts = pd.concat([counts, energy], axis=1).sort_values(0)
 
 # %%
-sns.scatterplot(
-    data=counts,
-    x=0,
-    y="Energy",
-    hue="PDGcode"
-    )
+plt.plot(counts['Energy'], counts[0], 'bo', markersize=0.5)
+plt.title('Relación entre nº de hits y Energía')
+plt.xlabel('Energía, E (GeV)')
+plt.ylabel('Nº de hits')
+plt.savefig('latex/img/scatter_n_e.pdf')
 # %%
 all_events.describe()
 # %%
 import numpy as np
 # %%
+import numpy as np
 ch_max = all_events['hitsCharge'].max() * 1.15
 
 
@@ -63,7 +77,14 @@ aux_2 = all_events['hitsCharge'] / ch_max
 aux_2.hist(histtype='step', bins=20, label='normal')
 aux = np.log(all_events['hitsCharge']) / np.log(ch_max)
 aux.hist(histtype='step', bins=20, label='log')
-
+#%%
+_ = plt.hist(aux, alpha=0.5, bins=20, label='log')
+_ = plt.hist(aux_2, alpha=0.5, bins=20, label='Normal')
+plt.title('Distribución de la carga por hit')
+plt.xlabel('$Q / Q_{max}$')
+plt.ylabel('Frecuencia')
+plt.legend()
+plt.savefig('latex/img/hist_charge.pdf')
 # %%
 c = []
 a = []
@@ -108,8 +129,13 @@ aux['Time'] = aux['Time0'] / aux['Time']
 ch_max = aux['hitsCharge'].max() * 1.15
 aux['hitsCharge_t'] = np.log(aux['hitsCharge']) / np.log(ch_max)
 # %%
+plt.figure(figsize=(7, 4))
 _ = plt.hist2d(aux['Time'], aux['hitsCharge_t'], bins=30)
-
+plt.title('Distribución de la carga de los hits a lo largo del tiempo')
+plt.colorbar()
+plt.xlabel('Tiempo / Tiempo$_{max}$')
+plt.ylabel('$\log(Q / Q_{max})$')
+plt.savefig('latex/img/hist_charge_time.pdf')
 # %%
 # start with a square Figure
 fig = plt.figure(figsize=(8, 8))
@@ -129,3 +155,8 @@ ax.hist2d(aux['Time'], aux['hitsCharge_t'], bins=20)
 # ax_histx.hist(aux['Time'])
 ax_histy.hist(aux['hitsCharge_t'])
 plt.show()
+# %%
+plt.hist(aux['hitsCharge_t'])
+
+# %%
+sns.jointplot(x=aux['Time'], y=aux['hitsCharge_t'], kind='hex')
