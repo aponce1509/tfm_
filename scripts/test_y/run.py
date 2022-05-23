bea0824fb989c47a06afe93b9440d996bd0a6d91
aponce1509 <mialumno@invidere.ugr.es>
2022-05-23 05:46:26 -0400

    pruebsa def representacion y

--- scripts/test_y/run.py
+++ scripts/test_y/run.py
@@ -13,133 +13,90 @@ def main(torch_rand=True):
 
 
 # modelo inicial
-# print("Modelo inicial")
-# params['n_epochs'] = 10
-# params["model_name"] = "convnet"
-# params['conv_filters'] = (16, 24)
-# params['conv_sizes'] = (3, 3)
-# params['conv_strides'] = (1, 1)
-# params['pool_sizes'] = (2, 2)
-# params['pool_strides'] = (2, 2)
-# params['clf_n_layers'] = (3)
-# params['clf_neurons'] = (32, 8, 2)
-# params['avg_layer'] = "identity"
-# params['avg_layer_size'] = None
-# for seed in params['seeds']:
-#     params['seed_'] = seed
-#     main()
+print("Modelo inicial")
+params['n_epochs'] = 10
+params["model_name"] = "convnet"
+params['conv_filters'] = (16, 24)
+params['conv_sizes'] = (3, 3)
+params['conv_strides'] = (1, 1)
+params['pool_sizes'] = (2, 2)
+params['pool_strides'] = (2, 2)
+params['clf_n_layers'] = (3)
+params['clf_neurons'] = (32, 8, 2)
+params['avg_layer'] = "identity"
+params['avg_layer_size'] = None
+params['batch_size'] = 48
+for seed in params['seeds']:
+    params['seed_'] = 19
+    # main()
 
-# # modelo denso
-# print("Modelo denso")
-# params["model_name"] = "convnet"
-# params['conv_filters'] = (32, 64, 64, 128)
-# params['conv_sizes'] = (3, 3, 3, 3)
-# params['conv_strides'] = (1, 1, 1, 1)
-# params['pool_sizes'] = (1, 2, 1, 2)
-# params['pool_strides'] = (1, 2, 1, 4)
-# params['clf_n_layers'] = (4)
-# params['clf_neurons'] = (50, 100, 50, 2)
-# params['optim_lr'] = 0.0001
-# for seed in params['seeds']:
-#     params['seed_'] = seed
-#     main()
+# modelo denso
+print("Modelo denso")
+params["model_name"] = "convnet"
+params['conv_filters'] = (32, 64, 64, 128)
+params['conv_sizes'] = (3, 3, 3, 3)
+params['conv_strides'] = (1, 1, 1, 1)
+params['pool_sizes'] = (1, 2, 1, 2)
+params['pool_strides'] = (1, 2, 1, 2)
+params['clf_n_layers'] = (4)
+params['clf_neurons'] = (50, 100, 50, 2)
+params['optim_lr'] = 0.0001
+params['batch_size'] = 72
+for seed in params['seeds']:
+    params['seed_'] = 19
+    # main()
 
-# print('Eff b0')
-# params["model_name"] = "effb0"
-# params['clf_n_layers'] = (4)
-# params['unfreeze_layers'] = 10
-# params['clf_neurons'] = (32, 8, 2)
-# params['transform'] = T.Compose([
-#     T.Lambda(lambda x: x.repeat(3, 1, 1)),
-#     # transforms.RandomResizedCrop((224, 224)),
-#     # transforms.RandomHorizontalFlip(),
-#     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# ])
-# params['optim_lr'] = 0.001
-# for seed in params['seeds']:
-#     params['seed_'] = seed
-#     main()
 
-# print('Eff b2')
-# params["model_name"] = "effb2"
-# params['clf_n_layers'] = (4)
-# params['unfreeze_layers'] = 10
-# params['clf_neurons'] = (32, 8, 2)
-# params['optim_lr'] = 0.001
-# params['transform'] = T.Compose([
-#     T.Lambda(lambda x: x.repeat(3, 1, 1)),
-#     # transforms.RandomResizedCrop((224, 224)),
-#     # transforms.RandomHorizontalFlip(),
-#     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# ])
-# for seed in params['seeds']:
-#     params['seed_'] = seed
-#     main()
-
-# print('Eff b5')
-# params["model_name"] = "effb5"
-# params['clf_n_layers'] = (4)
-# params['unfreeze_layers'] = 10
-# params['clf_neurons'] = (32, 8, 2)
-# params['optim_lr'] = 0.001
-# params['transform'] = T.Compose([
-#     T.Lambda(lambda x: x.repeat(3, 1, 1)),
-#     # transforms.RandomResizedCrop((224, 224)),
-#     # transforms.RandomHorizontalFlip(),
-#     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# ])
-# for seed in params['seeds']:
-#     params['seed_'] = seed
-#     main()
-
-print('Eff b7')
-params["model_name"] = "effb7"
+params['win_shape'] = (224, 224, 224)
+params['batch_size'] = 48
+params['scheduler_step_size'] = 3
+params['dropout'] = 0.2
 params['clf_n_layers'] = (4)
 params['unfreeze_layers'] = 10
 params['clf_neurons'] = (32, 8, 2)
-params['optim_lr'] = 0.0001
+params['optim_lr'] = 0.001
 params['transform'] = T.Compose([
     T.Lambda(lambda x: x.repeat(3, 1, 1)),
-    # transforms.RandomResizedCrop((224, 224)),
-    # transforms.RandomHorizontalFlip(),
     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
-params['batch_size'] = 16
+params['optim_lr'] = 0.001
+print('Eff b0')
+params["model_name"] = "effb0"
 for seed in params['seeds']:
-    params['seed_'] = seed
+    params['seed_'] = 19
+    # main()
+
+print('Eff b2')
+params["model_name"] = "effb2"
+for seed in params['seeds']:
+    params['seed_'] = 19
+    # main()
+
+print('Eff b5')
+params["model_name"] = "effb5"
+for seed in params['seeds']:
+    params['seed_'] = 19
+    # main()
+
+print('Eff b7')
+params["model_name"] = "effb7"
+for seed in params['seeds']:
+    params['seed_'] = 19
+    # main()
+
+print('ResNet')
+params["model_name"] = "resnet50"
+params['dropout'] = 0.3
+for seed in params['seeds']:
+    params['seed_'] = 19
+    # main()
+
+print('GoogleNet')
+params['dropout'] = 0.2
+params["model_name"] = "google"
+for seed in params['seeds']:
+    params['seed_'] = 19
     main()
-
-# print('ResNet')
-# params["model_name"] = "resnet50"
-# params['clf_n_layers'] = (4)
-# params['unfreeze_layers'] = 10
-# params['clf_neurons'] = (32, 8, 2)
-# params['optim_lr'] = 0.001
-# params['transform'] = T.Compose([
-#     T.Lambda(lambda x: x.repeat(3, 1, 1)),
-#     # transforms.RandomResizedCrop((224, 224)),
-#     # transforms.RandomHorizontalFlip(),
-#     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# ])
-# for seed in params['seeds']:
-#     params['seed_'] = seed
-#     main()
-
-# print('GoogleNet')
-# params["model_name"] = "google"
-# params['clf_n_layers'] = (4)
-# params['unfreeze_layers'] = 10
-# params['clf_neurons'] = (32, 8, 2)
-# params['optim_lr'] = 0.001
-# params['transform'] = T.Compose([
-#     T.Lambda(lambda x: x.repeat(3, 1, 1)),
-#     # transforms.RandomResizedCrop((224, 224)),
-#     # transforms.RandomHorizontalFlip(),
-#     T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# ])
-# for seed in params['seeds']:
-#     params['seed_'] = seed
-#     main()
 # from _models import ConvNet
 # from torchsummary import summary
 # model = ConvNet(params)
