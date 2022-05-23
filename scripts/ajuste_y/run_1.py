ba0abd3d7f257b4233c0888d1281ef3a2dd29d23
aponce1509 <mialumno@invidere.ugr.es>
2022-05-23 05:43:13 -0400

    mod. completa

--- scripts/ajuste_y/run_1.py
+++ scripts/ajuste_y/run_1.py
@@ -2,67 +2,70 @@ import sys
 sys.path.append('./scripts')
 from model_run import fix_seed, basic_pipeline, optuna_pipeline
 from exp_p_y.params import params
-exp_name = "Pruebas kernel simple"
+# exp_name = "Pruebas kernel simple"
+exp_name = 'Pruebas tamaño/resolución'
 
-def main_optuna(n_trials, exp_name, torch_rand=True):
-    for seed in params['seeds']:
-        params["seed_"] = seed
-        fix_seed(params, torch_rand)
-        optuna_pipeline(params, exp_name, n_trials=n_trials)
-
-params['grid'] = {"optimizer_grid":
-    {
-        "name": "optim_name",
-        "choices": ["adam"]
-    },
-    # "lr_grid": 
-    # {
-    #     "name": "lr",
-    #     "low": 3,
-    #     "high": 4
-    # },
-    'kernel_size':
-    {
-        'name': 'kernel_size',
-        'low': 3,
-        'high': 5
-    },
-    'kernel_stride':
-    {
-        'name': 'kernel_stride',
-        'low': 1,
-        'high': 4
-    },
-    'scheduler_gamma':
-    {
-        'name': 'gamma',
-        'low': 6,
-        'high': 10
-    }
-}
-
-params['n_epochs'] = 10
-params["model_name"] = "convnet"
-params['conv_filters'] = (16, 24)
-params['conv_sizes'] = (3, 3)
-params['conv_strides'] = (1, 1)
-params['pool_sizes'] = (2, 2)
-params['pool_strides'] = (2, 2)
-params['clf_n_layers'] = (3)
-params['clf_neurons'] = (32, 8, 2)
-params['seed'] = (19, )
-
-main_optuna(30, exp_name)
+def main(params, torch_rand=True):
+    fix_seed(params, torch_rand)
+    basic_pipeline(params, experiment_name=exp_name)
 
 # params['n_epochs'] = 10
-# params['seed'] = (2, 3, 19)
 # params["model_name"] = "convnet"
-# params['conv_filters'] = (32, 64, 64, 128)
-# params['conv_sizes'] = (3, 3, 3, 3)
-# params['conv_strides'] = (1, 1, 1, 1)
-# params['pool_sizes'] = (1, 2, 1, 2)
-# params['pool_strides'] = (1, 2, 1, 4)
-# params['clf_n_layers'] = (4)
-# params['clf_neurons'] = (50, 100, 50, 2)
+# params['conv_filters'] = (16, 24)
+# params['cube_shape_x'] = 1500
+# params['win_shape'] = (62, 62, 128)
+# params['pool_sizes'] = (2, 2)
+# params['pool_strides'] = (2, 2)
+# params['clf_n_layers'] = (3)
+# params['clf_neurons'] = (32, 8, 2)
+# params['seed'] = (19, )
+
+# kss = [5]
+# kds = [1, 2]
+# lrs = [0.001, 0.0001]
+# steps = [0.7, 0.8, 1]
+# for ks in kss:
+#     for kd in kds:
+#         for lr in lrs:
+#             for step in steps:
+#                 params['conv_sizes'] = (ks, ks)
+#                 params['conv_strides'] = (kd, kd)
+#                 params['optim_lr'] = lr
+#                 params['scheduler_gamma'] = step
+#                 main(params)
+
+
+params['n_epochs'] = 10
+params["model_name"] = "convnet "
+params['cube_shape_x'] = 1500
+params['win_shape'] = (62, 62, 128)
+params['conv_filters'] = (32, 64, 64, 128)
+params['pool_sizes'] = (1, 2, 1, 2)
+params['pool_strides'] = (1, 2, 1, 2)
+params['clf_neurons'] = (50, 100, 50, 2)
+params['batch_size'] = 74
+params['scheduler_step_size'] = 3
+params['seed'] = (19, )
+
+kss = [3, ]
+kds = [1, ]
+lrs = [0.0001]
+steps = [0.7, ]
+c_shapes = [250, 500, 1000, 1500, 2000]
+for ks in kss:
+    for kd in kds:
+        for lr in lrs:
+            for step in steps: 
+                for c_shape in c_shapes:
+                    if ks == 5:
+                        params['batch_size'] = 48
+                    elif ks == 3:
+                        params['batch_size'] = 74
+                    params['conv_sizes'] = (ks, ks, ks, ks)
+                    params['conv_strides'] = (kd, kd, kd, kd)
+                    params['optim_lr'] = lr
+                    params['cube_shape_x'] = c_shape
+                    params['scheduler_gamma'] = step
+                    main(params)
 
 # main_optuna(50)
